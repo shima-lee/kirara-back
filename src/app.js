664bb2df652980718251d5a2a467ae3816f8a4e4
shenghui.li <shenghui.li>
2021-06-24 11:16:11 +0800

    pre-lint testv1.0

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.use(session({
 }))
 
 // logger
-app.use(async (ctx, next) => {
+ app.use(async (ctx, next) => {
     const start = new Date()
     await next()
     const ms = new Date() - start
