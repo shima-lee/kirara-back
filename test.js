5f7e1b0dc5242c60678b9125ef77cbde11cac887
shenghui.li <shenghui.li>
2021-07-09 15:33:20 +0800

    feat: 个人主页 单元测试

--- test.js
+++
@@ -1,9 +0,0 @@
-let list = [{row:{rows:2}},{row:{rows:4}},{row:{rows:6}},{row:{rows:8}}]
-
-let result = list.map(item => item.row)
-
-expect = result.map(resultitem => {
-    resultitem.user = resultitem.rows
-})
-
-console.log(expect)
--- test/blog/blog.test.js
+++ test/blog/blog.test.js
@@ -34,3 +34,16 @@ test('创建微博，应成功', async () => {
     expect(res.body.data.content).toBe(content)
     expect(res.body.data.image).toBe(image)
 })
+
+test('个人主页，加载第一页数据应成功', async () => {
+    const res = await server
+        .get(`/api/profile/loadMore/zhangsan/0`)
+        .set('cookie', COOKIE)
+    expect(res.body.errno).toBe(0)
+    const data = res.body.data
+    expect(data).toHaveProperty('isEmpty')
+    expect(data).toHaveProperty('blogList')
+    expect(data).toHaveProperty('pageSize')
+    expect(data).toHaveProperty('pageIndex')
+    expect(data).toHaveProperty('count')
+})
