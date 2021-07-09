f3238351cdb5c671f715a5608a42ac3107fbf4af
shenghui.li <shenghui.li>
2021-07-09 16:28:59 +0800

    feat: 广场 单元测试

--- test/blog/blog.test.js
+++ test/blog/blog.test.js
@@ -47,3 +47,17 @@ test('个人主页，加载第一页数据应成功', async () => {
     expect(data).toHaveProperty('pageIndex')
     expect(data).toHaveProperty('count')
 })
+
+
+test('广场，加载第一页数据应成功', async () => {
+    const res = await server
+        .get(`/api/square/loadMore/0`)
+        .set('cookie', COOKIE)
+    expect(res.body.errno).toBe(0)
+    const data = res.body.data
+    expect(data).toHaveProperty('isEmpty')
+    expect(data).toHaveProperty('blogList')
+    expect(data).toHaveProperty('pageSize')
+    expect(data).toHaveProperty('pageIndex')
+    expect(data).toHaveProperty('count')
+})
